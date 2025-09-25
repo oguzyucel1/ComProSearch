9e808e34121bf837711d29f14bac35b492a594ce
oguzyucel1 <oguzyucell.oy@gmail.com>
2025-09-25 11:11:55 +0300

    sa

--- h.py
+++
@@ -1,9 +0,0 @@
-
-import cfscrape
-
-scraper = cfscrape.create_scraper()  
-response = scraper.get("https://www.oksid.com.tr/k/notebook/1798/")
-html = response.text
-
-print("Status:", response.status_code)
-print("Title:", html[:500])  # sadece ilk 500 karakteri yazdır
