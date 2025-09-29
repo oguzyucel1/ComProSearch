c69311ca96a24d72e3b27ce6a70535092a6313f9
oguzyucel1 <oguzyucell.oy@gmail.com>
2025-09-29 15:04:11 +0300

    selam

--- h.py
+++
@@ -1,5 +0,0 @@
-import cloudscraper
-
-scraper = cloudscraper.create_scraper()  # returns a CloudScraper instance
-# Or: scraper = cloudscraper.CloudScraper()  # CloudScraper inherits from requests.Session
-print(scraper.get("https://www.oksid.com.tr").text)  # => "<!DOCTYPE html><html><head>..."
